00bef2e597c412aeda44499f2d7f9554007d1412
cesarrodas <rodascesar03@yahoo.com>
2016-04-26 12:13:09 -0500

    Added Hot Module Loader for live reload of js

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 const merge = require('webpack-merge');
 
 const TARGET = process.env.npm_lifecycle_event;
@@ -7,7 +8,7 @@ const PATHS = {
   build: path.join(__dirname, 'build')
 };
 
-cons common = {
+const common = {
   // Entry accepts a path or an object of entries. We'll be using the
   // latter form given it's convenient with more complex configurations
   entry: {
@@ -22,7 +23,33 @@ cons common = {
 // Default configuration. We will return this if
 // Webpack is called outside of npm.
 if(TARGET === "start" || !TARGET) {
-  module.exports = merge(common, {});
+  module.exports = merge(common, {
+    devServer: {
+      contentBase: PATHS.build,
+      
+      // Enable history API fallback so HTML5 History API based routing works.
+      // This is a good default that will come in handy in more complicated setups.
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true,
+      
+      // Display only errors to reduce the amount of output.
+      stats: 'errors-only',
+      
+      // Parse host and port from env so this is easy to customize.
+      // If you use Vagrant or Cloud9, set
+      // host: process.env.HOST || '0.0.0.0';
+      // 
+      // 0.0.0.0 is available to all network devices unlike default
+      // localhost
+      host: process.env.HOST,
+      port: process.env.PORT
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
+    ]
+  });
 }
 
 if(TARGET === "build") {
